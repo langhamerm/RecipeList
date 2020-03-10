758637ae32511b05b7569cb66051afc43fe98847
Mark Langhamer <langhamerm@gmail.com>
2020-03-09 19:52:21 -0500

    Updates

---
+++ precache-manifest.6bd29a405a1e199548a6a83259adf1f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda0b33088f6bc7fbc8a",
+    "url": "/RecipeList/static/js/runtime~main.781352b2.js"
+  },
+  {
+    "revision": "35d1ac4bebbddc490f0d",
+    "url": "/RecipeList/static/js/main.a6e93851.chunk.js"
+  },
+  {
+    "revision": "883cbdef824e2c21e530",
+    "url": "/RecipeList/static/js/2.453da860.chunk.js"
+  },
+  {
+    "revision": "35d1ac4bebbddc490f0d",
+    "url": "/RecipeList/static/css/main.a2d7e02c.chunk.css"
+  },
+  {
+    "revision": "e4de91405c699b78ea3d8fe0950bd88a",
+    "url": "/RecipeList/index.html"
+  }
+];
